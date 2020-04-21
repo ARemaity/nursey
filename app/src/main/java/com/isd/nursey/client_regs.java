92450253d61a6c3bda4f73aba0802aedd049040a
ALI REMAITY <aliremaitybook@gmail.com>
2020-04-21 20:39:55 +0300

    init

--- app/src/main/java/com/isd/nursey/client_regs.java
+++ app/src/main/java/com/isd/nursey/client_regs.java
@@ -1,14 +1,14 @@
-  package com.isd.nursey;
+package com.isd.nursey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class client_regs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_client_regs);
     }
 }
