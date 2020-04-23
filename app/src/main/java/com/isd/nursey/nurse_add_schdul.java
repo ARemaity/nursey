35d9432bdfe1a1def8cb7a827844f1f2554d5119
ALI REMAITY <aliremaitybook@gmail.com>
2020-04-23 16:21:01 +0300

    add new activity structures

--- app/src/main/java/com/isd/nursey/nurse_add_schdul.java
+++ app/src/main/java/com/isd/nursey/nurse_add_schdul.java
@@ -1,14 +1,14 @@
-  package com.isd.nursey;
+package com.isd.nursey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class nurse_add_schdul extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nurse_add_schdul);
     }
 }
